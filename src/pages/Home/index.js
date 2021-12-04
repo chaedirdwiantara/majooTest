dd238be45a9dd18d1476c6585cda74d4ca263953
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-12-04 11:53:34 +0700

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { Button } from "../../components";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home-page-wrapper">
+      <div className="create-wrapper">apa kabar dunia</div>
+      <div className="content-wrapper">yo wassap</div>
+    </div>
+  );
+};
+
+export default Home;
